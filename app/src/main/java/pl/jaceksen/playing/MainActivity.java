2b628ee0efbabd0885bc5266c597ea3e11bb9194
jaceksen <jaceksen@gmail.com>
2017-01-07 22:25:27 +0100

    scrollbar end

--- app/src/main/java/pl/jaceksen/playing/MainActivity.java
+++ app/src/main/java/pl/jaceksen/playing/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        tu określamy jaki layout będzie widoczny
+//        layout jest w app/res/layout
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.note_layout);
     }
 }
